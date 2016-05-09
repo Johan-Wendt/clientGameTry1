8777e952657089396bde8d0a4fa21fa296a4f476
Johan Wendt <johan.wendt@telavox.se>
2016-05-09 07:47:08 +0200

    dd

--- public_html/index.js
+++ public_html/index.js
@@ -7,11 +7,19 @@ var rightCode = 39;
 var downCode = 40;
 var spaceCode = 32;
 var shootCode = 67;
+//p
 var pauseCode = 80;
+//j
+var joinCode = 74;
+//n
+var createCode = 78;
 
-var playerNumber;
 
-var smallestSquareSize = 14;
+//w
+var messageCode = 87;
+
+var playerNumber = 1;
+
 
 var CANVAS_WIDTH = 1000;
 var CANVAS_HEIGHT = 500;
@@ -28,6 +36,16 @@ var instructionsSentPerActor = 7;
 
 var fps = 60;
 
+var pistolBonus;
+var shotgunBonus;
+var player1;
+var player2;
+var fastBonus;
+var growBonus;
+var bullet;
+
+var playerName;
+
 
 
 window.onload = windowReady;
@@ -35,10 +53,13 @@ window.onload = windowReady;
 function windowReady() {
     context = document.getElementById('canvasId').getContext("2d");
     window.addEventListener("keydown", this.check, false);
+    loadImages();
     createPlayers();
     createBonuses();
     createBullets();
     setInterval(draw, 1000 / fps);
+
+    enterName();
 }
 
 ws.onopen = function () {
@@ -54,15 +75,41 @@ ws.onmessage = function (evt) {
 };
 
 ws.onclose = function () {
-    //  alert("Closed!");
+    sendAction(92);
+
 };
 
 ws.onerror = function (err) {
     alert("Error: " + err);
 };
+window.onbeforeunload = function () {
+    sendAction(92);
+    sleep(1000);
+}
+
+function loadImages() {
+    bullet = new Image(20, 9);
+    bullet.src = 'Pictures/AppleSeed.png';
+    growBonus = new Image(44, 59);
+    growBonus.src = 'Pictures/Mushroom.png';
+    fastBonus = new Image(46, 53);
+    fastBonus.src = 'Pictures/Coffee.png';
+    pistolBonus = new Image(40, 51);
+    pistolBonus.src = 'Pictures/Apple.png';
+    shotgunBonus = new Image(60, 33);
+    shotgunBonus.src = 'Pictures/Watermelon.png';
+    player1 = new Image(18, 23);
+    player1.src = 'Pictures/Player1.png';
+    player2 = new Image(18, 23);
+    player2.src = 'Pictures/Player2.png';
+}
 
 function check(e) {
     var code = e.keyCode;
+
+    // 1 = turn (1 = up, 2 = right, 3 = down, 4 = right) 
+    // 7 = text (1 = Name, 
+    // 9 = Join game (2:nd 
 
 
     switch (code) {
@@ -78,19 +125,30 @@ function check(e) {
         case leftCode:
             sendAction(14);
             break;
+        case pauseCode:
+            sendAction(25);
+            break;
         case spaceCode:
             sendAction(35);
             break;
         case shootCode:
             sendAction(45);
             break;
-        case pauseCode:
-            sendAction(25);
+
+        case joinCode:
+            sendAction(91);
+            break;
+        case createCode:
+            sendAction(81);
+            break;
+        case messageCode:
+            sendAction(71 + "Suck on this");
             break;
     }
 }
 
 function sendAction(actionNumber) {
+
     var action = playerNumber + "" + actionNumber;
     ws.send(action);
 }
@@ -107,27 +165,34 @@ function clearOldPositions(actor) {
 
 function handleIncommingData(evt) {
     var data = evt.data;
-    var view = new DataView(data);
-    var dataLength = view.byteLength;
-    var actionArrays = [];
-    var total = 0;
-    var lastSliced = 0;
-    while (total * 4 < dataLength) {
-        actionArrays[total] = view.getInt32(total * 4);
-        // console.log(actionArrays[total - 1]);
-        if (actionArrays[actionArrays.length - 1] == -1) {
-            if (total > lastSliced) {
 
-                handleSwitcher(actionArrays.slice(lastSliced, total));
-                lastSliced = total + 1;
+    if (data instanceof ArrayBuffer) {
+        var view = new DataView(data);
+
+        var dataLength = view.byteLength;
+        var actionArrays = [];
+        var total = 0;
+        var lastSliced = 0;
+        while (total * 4 < dataLength) {
+            actionArrays[total] = view.getInt32(total * 4);
+            if (actionArrays[actionArrays.length - 1] == -1) {
+                if (total > lastSliced) {
+
+                    handleSwitcher(actionArrays.slice(lastSliced, total));
+                    lastSliced = total + 1;
+                }
             }
+            total++;
         }
-        total++;
+    }
+    else {
+        handleGameMetaInfo(data);
     }
 }
 
 function pixel(I) {
-    I.color = I.col;
+    //   I.color = I.col;
+    I.image = I.image;
     I.x = [];
     I.y = [];
     I.width = [];
@@ -140,13 +205,9 @@ function pixel(I) {
         var n = 0;
 
         while (n < this.x.length) {
-            //context.fillRect(this.x[n] * smallestSquareSize + this.offsetX[n] / smallestSquareSize, this.y[n] * smallestSquareSize + this.offsetY[n] / smallestSquareSize, this.width, this.height);
-            // context.fillRect(this.x[n], this.y[n], this.width[n], this.height[n]);
             if (this.x[n] >= 0) {
-                drawRotatedRect(this.x[n], this.y[n], this.width[n], this.height[n], this.rotation[n] * 30);
-                // drawRotatedRect(x,y,width,height,degrees)
-                //    console.log("n = " + n);
-                //   console.log("this.x[n] = " + this.x[n]);
+                drawRotatedRect(this.image, this.x[n], this.y[n], this.image.width, this.image.height, this.rotation[n] * 30);
+
             }
             n++;
 
@@ -155,33 +216,37 @@ function pixel(I) {
     return I;
 }
 
-var gameBorders = {
-    color: "#CCC",
-    x: [],
-    y: [],
-    width: smallestSquareSize,
-    height: smallestSquareSize,
-    draw: function () {
-        context.fillStyle = this.color;
-        var n = 0;
+function drawRotatedRect(image, x, y, width, height, degrees) {
+    // first save the untranslated/unrotated context
+    context.save();
+
+    context.beginPath();
+    // move the rotation point to the center of the rect
+    //context.translate(x + width / 2, y + height / 2);
+    context.translate(x, y);
+    // rotate the rect
+    context.rotate(degrees * Math.PI / 180);
+
+    // draw the rect on the transformed context
+    // Note: after transforming [0,0] is visually [x,y]
+    //       so the rect needs to be offset accordingly when drawn
+    // context.fillStyle = "gold";
+    // context.fillRect(-width / 2.0, -height / 2.0, width, height);
+    context.drawImage(image, -width / 2.0, -height / 2.0, width, height);
+
+    // context.fill();
+
+    // restore the context to its untranslated/unrotated state
+    context.restore();
+
+}
 
 
-        while (n < this.x.length) {
-
-            context.fillRect(this.x[n] * smallestSquareSize, this.y[n] * smallestSquareSize, this.width, this.height);
-            n++;
-
-
-        }
-    }
-};
 
 function draw() {
-    // console.log("Called");
     context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     plays.forEach(function (pixel) {
         pixel.draw();
-        // console.log("Called again");
     });
     bonuses.forEach(function (pixel) {
         pixel.draw();
@@ -190,33 +255,17 @@ function draw() {
         pixel.draw();
     });
 
-    // gameBorders.draw();
-
 }
 
 function handleSwitcher(arr) {
-    //   printArr(arr);
-    // console.log("one round");
-
 
     var type = arr.shift();
-
-
-    // var happening = -1;
-    //while (arr.length > 0) {
-
-    //  var actInstructions = getNextActInstructions(arr);
 
     switch (type) {
 
 
         case 0:
-            if (!playerNumber) {
-                console.log("Player number before = " + playerNumber);
-                playerNumber = arr.shift();
-                console.log("Player number after = " + playerNumber);
-                console.log("done");
-            }
+            playerNumber = arr.shift();
             break;
         case 1:
             clearOldPositions(plays);
@@ -232,6 +281,9 @@ function handleSwitcher(arr) {
             break;
         case 4:
             handleWeaponInfo(arr);
+            break;
+        case 5:
+            handleGameMetaInfo(arr);
             break;
     }
     //}
@@ -275,60 +327,45 @@ function handleWeaponInfo(instructions) {
 
     }
 }
+function handleGameMetaInfo(instructions) {
 
 
-function handleGamePlan(arr) {
+    var type = instructions.substring(0,1);
+    console.log(type);
+    switch (type) {
 
-}
 
-
-function drawRotatedRect(x, y, width, height, degrees) {
-    // first save the untranslated/unrotated context
-    context.save();
-
-    context.beginPath();
-    // move the rotation point to the center of the rect
-    //context.translate(x + width / 2, y + height / 2);
-    context.translate(x, y);
-    // rotate the rect
-    context.rotate(degrees * Math.PI / 180);
-
-    // draw the rect on the transformed context
-    // Note: after transforming [0,0] is visually [x,y]
-    //       so the rect needs to be offset accordingly when drawn
-    // context.fillStyle = "gold";
-    context.fillRect(-width / 2.0, -height / 2.0, width, height);
-
-    // context.fill();
-
-    // restore the context to its untranslated/unrotated state
-    context.restore();
-
+        case "p":
+            //  playerNumber = arr.shift();
+            console.log(instructions);
+            break;
+    }
 }
 
 function createPlayers() {
-    plays.push(pixel({col: "#00A"}));
-    plays.push(pixel({col: "#AAA"}));
-    plays.push(pixel({col: "#123"}));
-    plays.push(pixel({col: "#987"}));
+    pistolBonus
+    plays.push(pixel({image: player1}));
+    plays.push(pixel({image: player2}));
+    plays.push(pixel({image: player1}));
+    plays.push(pixel({image: player1}));
 }
 
 function createBonuses() {
-    bonuses.push(pixel({col: "#F5A"}));
-    bonuses.push(pixel({col: "#FFA"}));
-    bonuses.push(pixel({col: "#123"}));
-    bonuses.push(pixel({col: "#987"}));
-    bonuses.push(pixel({col: "#555"}));
-    bonuses.push(pixel({col: "#1F7"}));
+    bonuses.push(pixel({image: growBonus}));
+    bonuses.push(pixel({image: fastBonus}));
+    bonuses.push(pixel({image: pistolBonus}));
+    bonuses.push(pixel({image: shotgunBonus}));
+    bonuses.push(pixel({image: shotgunBonus}));
+    bonuses.push(pixel({image: shotgunBonus}));
 
 }
 function createBullets() {
-    bullets.push(pixel({col: "#3F9"}));
-    bullets.push(pixel({col: "#3F9"}));
-    bullets.push(pixel({col: "#3F9"}));
-    bullets.push(pixel({col: "#3F9"}));
-    bullets.push(pixel({col: "#3F9"}));
-    bullets.push(pixel({col: "#3F9"}));
+    bullets.push(pixel({image: bullet}));
+    bullets.push(pixel({image: bullet}));
+    bullets.push(pixel({image: bullet}));
+    bullets.push(pixel({image: shotgunBonus}));
+    bullets.push(pixel({image: shotgunBonus}));
+    bullets.push(pixel({image: shotgunBonus}));
 
 }
 function getWeaponName(weaponInt) {
@@ -351,4 +388,20 @@ function printArr(array) {
         console.log(element);
     });
     console.log("done printing array");
+}
+
+function enterName() {
+    var playerName = prompt("Please enter your name", "New Player");
+    sendAction(71 + playerName);
+
+
+}
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
+    }
 }
